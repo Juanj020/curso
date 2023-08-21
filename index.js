8d1693f6058e07f9f81b7bf19834500e2dcdf6a9
Juan Jose Lizarazo <juanj202122@gmai.com>
2023-08-21 17:42:37 -0500

    terminada la semana 1

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
 const http = require('http');
-const { dirname } = require('path');
-const url = require('url')
+const url = require('url');
+
+/* Es lo que va al final de la url, es como un indicador más breve xd */
+const slugify = require('slugify')
+
+const replaceTemplate = require('./modules.js/replaceTemplate.js')
 
 /* -------------------- manera sincrona --------------- */
 /* fs: Significa  - File System */
@@ -37,26 +41,47 @@ console.log('Buena amigos de youtube'); */
 
 /* --------------------------------------------- */
 /* ------------- PRIMER SERVIDOR --------------- */
+
 /* Codigo de nivel superior es decir solo se ejecuta una vez y hay muere xd, no se reinicia ni nada */
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
 /* EL punto "." es donde se ejecuta la acción y el __dirname es donde se encuentra la carpeta actual */
 const dataObject = JSON.parse(data)
 
+/* const slugs = dataObject.map(ele => slugify(ele.productName, { lower: true}))
+console.log(slugs); */
+
+console.log(slugify('Aguacates frescos xd', { lower: true}));
+
 const server =  http.createServer((req,res)=>{
-    console.log(req.url);
+    
+    const { query, pathname } = url.parse(req.url, true)
 
-    const pathName = req.url
+    if(pathname === "/" || pathname === "/overview" ){
+        res.writeHead(200, {'Content-type':'text/html'});
 
-    if(pathName === "/" || pathName === "/overview" ){
-        res.end('Hola mi so estamos en overview');
-    }else  if(pathName === "/product"){
-        res.end('Hola mi so estamos en el producto');
-    }else if(pathName === "/api"){
+        const cardsHtml = dataObject.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+
+        res.end(output);
+
+    }else  if(pathname === "/product"){
+        res.writeHead(200, {'Content-type':'text/html'});
+
+        const product = dataObject[query.id];
+        const output = replaceTemplate(tempProduct, product)
+        res.end(output);
+
+
+    }else if(pathname === "/api"){
         res.writeHead(200, {'Content-type':'application/json'})
         res.end(data);
     }else{
         /* Un encabezado http wa basicamente una información sobre la respuesta que estamos enviando */
-        res.writeHead(404,{
+        res.writeHead(404,'utf-8',{
             'Content-type':'text/html',
             'my-own-header': 'hello-word'
         });
